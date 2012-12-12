95540cdf083923c492ef5972337005f3d8e203f0
grigory <grigory@grigory-notebook.(none)>
2012-12-13 01:08:27 +0400

    Клиент для игры. Двигаем шарик и площадку Отправляем данные о площадке на сервер

--- static/js/game.js
+++ static/js/game.js
@@ -1,10 +1,17 @@
 // JavaScript Document
-var x = 50;
-var y = 50;
+var boardPos = 0;
 $(document).ready(function() {
-		/*
-		setInterval(
-		$.get(), 50);
-		*/
-		$('#game_space').css('background-position', x + "px " + y + "px");
+		$('#game_space').css('background-position -30px -30px');
+		setInterval(function() {
+			$.get("/updateGameData", {'boardPos', boardPos}, function(data) {
+	      	var x = data.ballXPos;
+	      	var y = data.ballYPos;
+				$('#game_space').css('background-position', x + "px " + y + "px");            
+	      }, 'json');    
+   	}, 25);
+
+		$("#game_space").mousemove(function(e){
+			boardPos = e.pageX - $("#me").offset().left - 25;
+			$("#me").css('background-position', boardPos + "px " + 0 + "px");
+   	}); 
 });
