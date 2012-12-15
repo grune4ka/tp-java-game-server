d136340405ff4db7db84b866897d551e4830a12a
grigory <grigory@grigory-notebook.(none)>
2012-12-15 04:34:56 +0400

    плавное перемещение шарика в chrome

--- static/js/game.js
+++ static/js/game.js
@@ -1,18 +1,41 @@
-// JavaScript Document
+// JavaScript 5cument
 var boardPos = 0;
 $(document).ready(function() {
-		$('#game_space').css('background-position -30px -30px');
+		$('#game_space').css("background-position", "-30px -30px");
 		setInterval(function() {
 			$.get("/updateGameData", {'boardPos' : boardPos}, function(data) {
-	      	var x = data.ballXPos;
-	      	var y = data.ballYPos;
-				$('#game_space').css('background-position', x + "px " + y + "px");
-				$('#enemy').css('background-position', data.enemyPosition + "px " +  10 + "px");                        
-	      }, 'json');    
-   	}, 25);
+				if (data.results == "true") {
+					window.location = "/results?me=" + data.yourPoints + "&enemy=" + data.enemyPoints;
+					return;
+				}
+				var x = data.ballXPos;
+	      	var y = data.ballYPos;	      	
 
-		$("#game_space").mousemove(function(e){
+				if ($.browser.webkit == true) {
+					$('#game_space').animate({"background-position-x" : x, "background-position-y" : y}, 20, "linear");
+					$('#enemy').css('background-position', data.enemyPosition + "px " +  20 + "px");             
+				}
+				else {
+					$('#game_space').css('background-position', x + "px " + y + "px");
+					$('#enemy').css('background-position', data.enemyPosition + "px " +  20 + "px");             
+				}
+				$('#enemyPoints').html(data.enemyPoints);
+				$('#yourPoints').html(data.yourPoints);
+	      }, 'json');    
+   	}, 40);
+
+		$(document).mousemove(function(e){
 			boardPos = e.pageX - $("#me").offset().left - 25;
-			$("#me").css('background-position', boardPos + "px " + 0 + "px");
+			if (boardPos < 100 ) {
+				boradPos = 100;
+				$("#me").css('background-position', boardPos + "px " + 5 + "px");
+				return;
+			}
+			if (boardPos > 315) {
+				boardPos = 315;
+				$("#me").css('background-position', boardPos + "px " + 5 + "px");
+				return;
+			}
+			$("#me").css('background-position', boardPos + "px " + 5 + "px");
    	}); 
 });
